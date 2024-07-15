7711c6159bb4fc015082d963c0c172a38f623e7b
Rahul Singh <142200955+Rahulsing12345@users.noreply.github.com>
2024-07-15 09:53:23 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,57 @@
+// Challange  1
+
+
+
+// Declare a variable using var keyword, assign it a number, and log the value to the console
+
+var numberOfCoder = 1
+console.log(`Total number of coders are ${numberOfCoder}`);
+
+
+
+
+// Declare a variable using let keyword, assign it a string, and log the value to the console
+
+let fullName = "Rahul Singh"
+console.log(`My full-Name is ${fullName}`);
+fullName = "Bobby Singh"                                                                   //   Re-assign the value
+console.log(`My updated full-Name is ${fullName}`);
+
+
+// Declare a variable using const keyword, assign it a boolean value, and log the value to the console
+
+const isLoggedIn = true
+console.log(isLoggedIn);
+// isLoggedIn = false
+// console.log(isLoggedIn);       
+                          
+// NOTE:     if we re-assign in const it through error because const keyword is immutable
+
+
+
+// Create  variables of different data types ( number, string boolean, object, array) and log each variables
+
+let num = 6
+console.log(typeof(num));
+
+let favSubject = "DBMS"
+console.log(typeof(favSubject));
+
+let matchPattern = false
+console.log(typeof(matchPattern));
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(typeof(fruits));                                                         // The "typeof" operator in JavaScript returns "object" for arrays.
+
+const personalDetail = {firstName : "Rahul" , lastName : "Singh" , age:50 }
+console.log(personalDetail.firstName);
+console.log(typeof(personalDetail)); 
+
+
+
+
+
+
+
+
+
