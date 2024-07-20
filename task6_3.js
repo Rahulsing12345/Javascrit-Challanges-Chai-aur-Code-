2863ded87feb424f846f4691c1e15995b3705567
Rahul Singh <142200955+Rahulsing12345@users.noreply.github.com>
2024-07-20 10:06:49 +0530

    Add files via upload

---
+++ task6_3.js
@@ -0,0 +1,44 @@
+// Array iteration     {for and forEach loops}
+
+// Use a for loop to iterate over the array and log each element to the console.
+
+const myArray = ["BMW", "SAFARI", "Audi", "MARUTI", "ALTO", "NANO"];
+
+for (let i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
+}
+
+//OUTPUT:
+
+// BMW
+// SAFARI
+// Audi
+// MARUTI
+// ALTO
+// NANO
+
+
+// Use the forEach method to iterate over the array and log each element to the console.
+
+const arrayNum = [1, 2, 3, 55, 88, 45, 78, 10];
+arrayNum.forEach(element => {
+    console.log(element);
+});
+
+//OUTPUT :
+
+// 1
+// 2
+// 3
+// 55
+// 88
+// 45
+// 78
+// 10
+
+
+
+
+
+
+
