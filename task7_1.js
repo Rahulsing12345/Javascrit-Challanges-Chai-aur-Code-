260f11f99064528aae0479bf7f64200da3d03266
Rahul Singh <142200955+Rahulsing12345@users.noreply.github.com>
2024-07-21 12:04:48 +0530

    Add files via upload

---
+++ task7_1.js
@@ -0,0 +1,26 @@
+// Objects
+// Object Creation and access
+
+// Create an object representing an book with properties like Title, Author, and Year, and log the object to the console.
+
+const myBookObj = {
+    title : "Mathematics",
+    author : "RD Sharma",
+    Year : "2004"
+}
+console.log(myBookObj);
+
+// OUTPUT : 
+// { title: 'Mathematics', author: 'RD Sharma', Year: '2004' }
+
+const bookObject = {}
+bookObject.title = "Science"
+bookObject.author = "S Chand"
+bookObject.Year = "1958"
+
+console.log(bookObject);
+
+// OUTPUT : 
+// { title: 'Science', author: 'S Chand', Year: '1958' }
+
+
